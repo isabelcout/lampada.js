090de9d1cb30ff42564cf736c9761c836ce070a7
Isabel Coutinho <maria.2225506@aluno.faculdadedescomplica.com.br>
2023-09-08 16:45:40 -0300

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+let lampada = document.querySelector("img");
+
+function lampadaAcesa() {
+  lampada.src =
+    "https://1.bp.blogspot.com/-k6xq_xeYB68/VxAc4MEifiI/AAAAAAAAAXQ/b9W3g0846oE8UrCQGWzjz8BmFJ7J3g1bwCKgB/s1600/lampada_acesa.png";
+}
+
+function lampadaApagada() {
+  lampada.src = "https://mariaccarolina.github.io/L-mpada/img/lamp.jpg";
+}
